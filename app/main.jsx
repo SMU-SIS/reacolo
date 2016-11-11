3d1fb20378e38a48acf1651697fc33e88984a1b8
Quentin Roy <quentin@quentinroy.fr>
2016-11-11 15:49:29 +0800

    feat: Set up role from querystring.

--- app/main.jsx
+++ app/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import querystring from 'querystring';
 import App from './components/App';
 import ModelSync from './dirty-model-sync';
 
+const pageArgs = querystring.parse(window.location.search.substring(1));
 const contentDiv = document.getElementById('content');
-const modelSync = new ModelSync(`http://${location.hostname}:3000/socket`, 'app');
+const modelSync = new ModelSync(`http://${location.hostname}:3000/socket`, pageArgs.role);
 const setData = (...args) => modelSync.setAppData(...args);
 
 modelSync.onUpdate = (data, context) => {
