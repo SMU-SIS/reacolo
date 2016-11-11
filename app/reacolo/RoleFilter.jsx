927fca84134741e064afe4e0af4e2e2a7b6f8273
Quentin Roy <quentin@quentinroy.fr>
2016-11-11 18:18:35 +0800

    feat: Implement role filter.

---
+++ app/reacolo/RoleFilter.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ComponentFilter from './ComponentFilter';
+
+const arrayIncludes = Array.prototype.includes;
+
+export default function RoleFilter(props) {
+  let shouldRender;
+  if (typeof props.target === 'string') {
+    shouldRender = props.target === props.context.role;
+  } else {
+    shouldRender = arrayIncludes.call(props.target, props.context.role);
+  }
+  return (
+    <ComponentFilter rendered={shouldRender}>
+      {props.children}
+    </ComponentFilter>
+  );
+}
+
+RoleFilter.propTypes = {
+  target: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.arrayOf(React.PropTypes.string)
+  ]).isRequired,
+  children: React.PropTypes.element.isRequired,
+  context: React.PropTypes.shape({
+    role: React.PropTypes.string.isRequired
+  })
+};
