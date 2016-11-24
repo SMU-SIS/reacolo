c9d096034a8e9843a7bb8d34147d11898c2a17db
Quentin Roy <quentin@quentinroy.fr>
2016-11-24 11:53:47 +0800

    refactor: Remove the demo and make this repository a reacolo library instead of an app.

--- app/reacolo/index.js
+++
@@ -1,5 +0,0 @@
-import EcologyFilter from './EcologyFilter';
-import RoleFilter from './RoleFilter';
-import contextPropType from './context-prop-type';
-
-export { EcologyFilter, RoleFilter, contextPropType };
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,35 @@
 var webpack = require('webpack');
 var path = require('path');
-var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true,
-    contentBase: './app',
-    port: 8080
-  },
-  entry: [
-    'webpack/hot/dev-server',
-    'webpack-dev-server/client?http://localhost:8080',
-    path.resolve(__dirname, 'app/main.jsx')
-  ],
+  devtool: "source-map",
+  entry: [ path.resolve(__dirname, 'lib/index.js') ],
   output: {
-    path: __dirname + '/build',
-    publicPath: '/',
-    filename: './bundle.js'
+    path: path.join(__dirname, 'dist'),
+    filename: 'reacolo.js',
+    libraryTarget: 'umd',
+    library: 'reacolo'
+  },
+  externals: {
+      react: { root: 'React', amd: 'react', commonjs: 'react', commonjs2: 'react' },
+      'sockjs-client': {
+        root: 'SockJS',
+        amd: 'sockjs-client',
+        commonjs: 'sockjs-client',
+        commonjs2: 'sockjs-client'
+      }
   },
   module: {
     loaders:[
-      { test: /\.css$/, include: path.resolve(__dirname, 'app'), loader: 'style-loader!css-loader' },
-      { test: /\.js[x]?$/, include: path.resolve(__dirname, 'app'), exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.js[x]?$/,
+        include: path.resolve(__dirname, 'lib'),
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
     ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new OpenBrowserPlugin({ url: 'http://localhost:8080' })
-  ]
+  }
 };
