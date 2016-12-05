f9acf17d70f34c8996f102834b784102eecb227d
Quentin Roy <quentin@quentinroy.fr>
2016-12-05 19:17:54 +0800

    feat: Update to the new server protocol and separate the server sync to the rest of the library

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
 
-module.exports = {
+const base = {
   devtool: 'source-map',
-  entry: path.resolve(__dirname, 'lib/index.js'),
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'reacolo.js',
-    libraryTarget: 'umd',
-    library: 'reacolo'
-  },
-  externals: {
-      react: { root: 'React', amd: 'react', commonjs: 'react', commonjs2: 'react' },
-      'sockjs-client': {
-        root: 'SockJS',
-        amd: 'sockjs-client',
-        commonjs: 'sockjs-client',
-        commonjs2: 'sockjs-client'
-      }
-  },
   module: {
     rules: [
       {
@@ -33,3 +17,49 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   }
 };
+
+module.exports = [
+  // reacolo
+  Object.assign({}, base, {
+    entry: path.resolve(__dirname, 'lib/reacolo'),
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'reacolo.js',
+      libraryTarget: 'umd',
+      library: 'reacolo'
+    },
+    externals: {
+      react: {
+        root: 'React',
+        amd: 'react',
+        commonjs: 'react',
+        commonjs2: 'react'
+      }
+    }
+  }),
+
+  // reacolo-dirty-model-sync
+  Object.assign({}, base, {
+    entry: path.resolve(__dirname, 'lib/dirty-model-sync'),
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'reacolo-dirty-model-sync.js',
+      libraryTarget: 'umd',
+      library: 'ReacoloModelSync'
+    },
+    externals: {
+      'sockjs-client': {
+        root: 'SockJS',
+        amd: 'sockjs-client',
+        commonjs: 'sockjs-client',
+        commonjs2: 'sockjs-client'
+      },
+      eventemitter3: {
+        root: 'EventEmitter',
+        amd: 'eventemitter3',
+        commonjs: 'eventemitter3',
+        commonjs2: 'eventemitter3'
+      }
+    }
+  })
+];
