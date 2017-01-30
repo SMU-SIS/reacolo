8b6095467e1c0bc2b5b1f7bceb7c9badf85a4ea9
Quentin Roy <quentin@quentinroy.fr>
2017-01-30 19:18:57 +0800

    feat(ReacoloFilter): New ReacoloFilter that comply with the new API.

---
+++ lib/__tests__/ReacoloFilter.test.jsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ReacoloFilter from '../reacolo/ReacoloFilter';
+
+describe('ReacoloFilter#wouldRender with roles', () => {
+  it('is rendered when all roles are here', () => {
+    const roles = 'role1 | role2 & role3 & role4?';
+    const context = {
+      roles: {
+        role2: 2,
+        role3: 1,
+        role4: 0
+      }
+    };
+    expect(ReacoloFilter.wouldRender(context, { roles })).toBe(true);
+  });
+  it('is rendered when all roles are here (pre-decoded)', () => {
+    const roles = [{
+      role1: { optional: false }
+    }, {
+      role2: { optional: false },
+      role3: { optional: false },
+      role4: { optional: true },
+    }];
+    const context = {
+      roles: {
+        role2: 2,
+        role3: 1,
+        role4: 0
+      }
+    };
+    expect(ReacoloFilter.wouldRender(context, { roles })).toBe(true);
+  });
+  it('is not rendered when there are missing roles', () => {
+    const roles = 'role1 & role2 & role3? | role2 & role3 | role4';
+    const context = {
+      roles: {
+        role1: 2,
+        role2: 0,
+        role3: 1
+      }
+    };
+    expect(ReacoloFilter.wouldRender(context, { roles })).toBe(false);
+  });
+  it('is not rendered when there are missing roles (pre-decoded)', () => {
+    const roles = [
+      { role1: { optional: false } },
+      { role2: { optional: false } },
+      { role3: { optional: true } }
+    ];
+    const context = {
+      roles: {
+        role1: 2,
+        role2: 0,
+        role3: 1
+      }
+    };
+    expect(ReacoloFilter.wouldRender(context, { roles })).toBe(false);
+  });
+});
+
+describe('ReacoloFilter#wouldRender with activity', () => {
+  it('is rendered when the activity is one of the targets', () => {
+    const activity = 'talking|walking';
+    const context = {
+      activity: 'walking'
+    };
+    expect(ReacoloFilter.wouldRender(context, { activity })).toBe(true);
+  });
+  it('is rendered when the activity is one of the targets (pre-decoded)', () => {
+    const activity = [
+      { talking: { optional: false } },
+      { walking: { optional: false } }
+    ];
+    const context = {
+      activity: 'walking'
+    };
+    expect(ReacoloFilter.wouldRender(context, { activity })).toBe(true);
+  });
+  it('is not rendered when the activity is not one of the targets', () => {
+    const activity = 'talking|walking';
+    const context = {
+      activity: 'jogging'
+    };
+    expect(ReacoloFilter.wouldRender(context, { activity })).toBe(false);
+  });
+  it('is not rendered when the activity is not one of the targets (pre-decoded)', () => {
+    const activity = [
+      { talking: { optional: false } },
+      { walking: { optional: false } }
+    ];
+    const context = {
+      activity: 'jogging'
+    };
+    expect(ReacoloFilter.wouldRender(context, { activity })).toBe(false);
+  });
+});
+
+describe('ReacoloFilter#wouldRender with clientRole', () => {
+  it('is rendered when the clientRole is one of the targets', () => {
+    const clientRole = 'role1|role2|role3';
+    const context = {
+      clientRole: 'role2'
+    };
+    expect(ReacoloFilter.wouldRender(context, { clientRole })).toBe(true);
+  });
+  it('is rendered when the clientRole is one of the targets (pre-decoded)', () => {
+    const clientRole = [
+      { role1: { optional: false } },
+      { role2: { optional: false } },
+      { role3: { optional: false } }
+    ];
+    const context = {
+      clientRole: 'role2'
+    };
+    expect(ReacoloFilter.wouldRender(context, { clientRole })).toBe(true);
+  });
+  it('is not rendered when the clientRole is not one of the targets', () => {
+    const clientRole = 'role1|role3';
+    const context = {
+      clientRole: 'role2'
+    };
+    expect(ReacoloFilter.wouldRender(context, { clientRole })).toBe(false);
+  });
+  it('is not rendered when the clientRole is not one of the targets (pre-decoded)', () => {
+    const clientRole = [
+      { role1: { optional: false } },
+      { role3: { optional: false } }
+    ];
+    const context = {
+      clientRole: 'role2'
+    };
+    expect(ReacoloFilter.wouldRender(context, { clientRole })).toBe(false);
+  });
+});
+
+describe('ReacoloFilter', () => {
+  const originalWouldRender = ReacoloFilter.wouldRender;
+  beforeEach(() => {
+    ReacoloFilter.wouldRender = jest.fn(() => true);
+  });
+  afterEach(() => {
+    ReacoloFilter.wouldRender = originalWouldRender;
+  });
+  it('properly calls ReacoloFilter#wouldRender', () => {
+    renderer.create(
+      <ReacoloFilter
+        activity="test-activity"
+        clientRole="test-clientRole"
+        roles="test-roles"
+        context={{ uselessProp: 'just for the test', roles: { role1: 2 } }}
+      >
+        <div id="child" />
+      </ReacoloFilter>
+    );
+    expect(ReacoloFilter.wouldRender.mock.calls).toEqual([[{
+      uselessProp: 'just for the test',
+      roles: { role1: 2 }
+    }, {
+      activity: 'test-activity',
+      clientRole: 'test-clientRole',
+      roles: 'test-roles'
+    }]]);
+  });
+  it('renders if ReacoloFilter#wouldRender returns true', () => {
+    ReacoloFilter.wouldRender.mockImplementation(() => true);
+    const component = renderer.create(
+      <ReacoloFilter context={{ roles: {} }}>
+        <div id="child" />
+      </ReacoloFilter>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+  it('does not render if ReacoloFilter#wouldRender returns false', () => {
+    ReacoloFilter.wouldRender.mockImplementation(() => false);
+    const component = renderer.create(
+      <ReacoloFilter context={{ roles: {} }}>
+        <div id="child" />
+      </ReacoloFilter>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
--- lib/reacolo/ReacoloFilter.jsx
+++ lib/reacolo/ReacoloFilter.jsx
@@ -1,42 +1,55 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import ComponentFilter from './ComponentFilter';
 import contextPropType from './context-prop-type';
-import { contextFilter, valueType, countType } from './filtering/context-filter';
+import valueGroupsFilter from './filtering/value-groups-filter';
+import decodeValueGroups from './filtering/decode-targets';
 
-const ReacoloFilter = (props) => {
-  // Build the target props representation to be checked.
-  const targetProps = [
-    { name: 'activity', value: props.activity, type: valueType },
-    { name: 'clientRole', value: props.clientRole, type: valueType },
-    { name: 'roles', value: props.roles, type: countType },
-  ];
-  // Check the context.
-  const rendered = contextFilter(props.context, targetProps);
-  return (
-    <ComponentFilter rendered={rendered}>
-      { props.children }
-    </ComponentFilter>
-  );
-};
+class ReacoloFilter extends PureComponent {
+  static wouldRender(context, targets) {
+    // Build the value / target map. Adapts the context values so that they are all an dictionary
+    // of count.
+    const valueTargetMap = [
+      [(context.roles ? context.roles : {}), targets.roles],
+      [(context.activity ? { [context.activity]: 1 } : {}), targets.activity],
+      [(context.clientRole ? { [context.clientRole]: 1 } : {}), targets.clientRole]
+    ];
+    // If the decoded boolean is true,
+    return valueTargetMap.every(([values, targetGroup]) => {
+      const coded = !targetGroup || typeof targetGroup === 'string';
+      // If the target group is encoded, decode it.
+      const decodedTargetGroup = coded ? decodeValueGroups(targetGroup) : targetGroup;
+      // Check the filter on this target / value pair.
+      return valueGroupsFilter(values, decodedTargetGroup);
+    });
+  }
+  render() {
+    const { roles, activity, clientRole } = this.props;
+    const shouldRender = this.constructor.wouldRender(
+      this.props.context,
+      { roles, activity, clientRole }
+    );
+    return (
+      <ComponentFilter rendered={shouldRender}>
+        { this.props.children }
+      </ComponentFilter>
+    );
+  }
+}
 
+/* eslint-disable react/no-unused-prop-types */
 ReacoloFilter.propTypes = {
   context: contextPropType.isRequired,
-  children: React.node.isRequired,
-  roles: React.PropTypes.oneOfType([
-    React.PropTypes.arrayOf(React.PropTypes.string),
-    React.PropTypes.arrayOf(React.PropTypes.arrayOf(React.PropTypes.string))
-    // TODO:
-    // - React.PropTypes.objectOf(React.PropTypes.number)
-    // - React.PropTypes.arrayOf(React.PropTypes.objectOf(React.PropTypes.number))
-  ]),
+  children: React.PropTypes.node.isRequired,
+  roles: React.PropTypes.string,
   activity: React.PropTypes.string,
   clientRole: React.PropTypes.string
 };
+/* eslint-enable react/no-unused-prop-types */
 
 ReacoloFilter.defaultProps = {
-  roles: ['*'],
-  activity: '*',
-  clientRole: '*'
+  roles: undefined,
+  activity: undefined,
+  clientRole: undefined
 };
 
 export default ReacoloFilter;
