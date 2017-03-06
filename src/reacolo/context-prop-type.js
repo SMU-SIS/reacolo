c203bd5b32552932cf3931ed37aa7bf2284d5069
Quentin Roy <quentin@quentinroy.fr>
2017-03-06 19:04:33 +0800

    fix: Remove removed property from context-prop-type.

--- src/reacolo/context-prop-type.js
+++ src/reacolo/context-prop-type.js
@@ -2,6 +2,5 @@ import React from 'react';
 
 export default React.PropTypes.shape({
   clientRole: React.PropTypes.string,
-  activity: React.PropTypes.string,
   roles: React.PropTypes.objectOf(React.PropTypes.number).isRequired
 });
