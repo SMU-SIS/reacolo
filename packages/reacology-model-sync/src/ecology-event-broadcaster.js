5da3331836957030949514441576942b1d304b49
Quentin Roy <quentin@quentinroy.fr>
2017-08-11 14:25:21 +0800

    fix(reacology-model-sync): Fix crash on import and server initialization.

--- packages/reacology-model-sync/src/ecology-event-broadcaster.js
+++ packages/reacology-model-sync/src/ecology-event-broadcaster.js
@@ -1,17 +1,15 @@
 /* global cordova */
-// Fetch the cordova ecology plugin
-const CORDOVA_ECOLOGY = cordova.plugins.CordovaEcology;
 
 export default class EcologyEventBroadcaster {
   subscribe(eventType, listener) {
-    CORDOVA_ECOLOGY.subscribeEvent(eventType, listener);
+    cordova.plugins.CordovaEcology.subscribeEvent(eventType, listener);
   }
 
   unsubscribe(eventType, listener) {
-    CORDOVA_ECOLOGY.unsubscribeEvent(eventType, listener);
+    cordova.plugins.CordovaEcology.unsubscribeEvent(eventType, listener);
   }
 
   publish(eventType, eventData) {
-    CORDOVA_ECOLOGY.publishEvent(eventType, eventData);
+    cordova.plugins.CordovaEcology.publishEvent(eventType, eventData);
   }
 }
