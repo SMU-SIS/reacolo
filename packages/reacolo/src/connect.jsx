c4518b0eb2f5f311ff198cc3397a9d66fbad1ff6
Quentin Roy <quentin@quentinroy.fr>
2017-08-17 20:53:54 +0800

    refactor(reacolo): `connect` does not use ConnectHOC anymore

--- packages/reacolo/src/connect.jsx
+++ packages/reacolo/src/connect.jsx
@@ -1,8 +1,77 @@
-import React from 'react';
-import ConnectHOC from './ConnectHOC.jsx';
+import React, { Component } from 'react';
 
-const connect = (Component, model) => props => (
-  <ConnectHOC {...props} model={model} Component={Component} />
-);
+const attachModelHandlers = (model, handlers) => {
+  handlers.forEach(([evt, handler]) => {
+    model.addListener(evt, handler);
+  });
+};
+
+const detachModelHandlers = (model, handlers) => {
+  handlers.forEach(([evt, handler]) => {
+    model.removeListener(evt, handler);
+  });
+};
+
+const connect = (WrappedComponent, model) => {
+  const setData = model.setAppData.bind(model);
+
+  class ConnectedComponent extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: model.data || {},
+        context: model.context || {},
+        isConnected: model.isConnected
+      };
+
+      // Create the handlers.
+      this._modelHandlers = [
+        [Symbol.for('connected'), () => this.updateStateFromModel()],
+        [Symbol.for('disconnected'), () => this.updateStateFromModel()],
+        [
+          Symbol.for('data:update'),
+          newData => this.setState({ data: newData })
+        ],
+        [
+          Symbol.for('context:update'),
+          newContext => this.setState({ context: newContext })
+        ]
+      ];
+    }
+
+    componentDidMount() {
+      attachModelHandlers(model, this._modelHandlers);
+      this.updateStateFromModel();
+    }
+
+    componentWillUnmount() {
+      detachModelHandlers(model, this._modelHandlers);
+    }
+
+    updateStateFromModel() {
+      const isConnected = model.isConnected;
+      this.setState({
+        data: isConnected ? model.data : undefined,
+        context: model.context,
+        isConnected
+      });
+    }
+
+    render() {
+      const { data, context, isConnected } = this.state;
+      return (
+        <WrappedComponent
+          data={data}
+          context={context}
+          isConnected={isConnected}
+          setData={setData}
+          {...this.props}
+        />
+      );
+    }
+  }
+
+  return ConnectedComponent;
+};
 
 export default connect;
