e8f77d8c7809d560c63e27faecb1e52700f760bf
Quentin Roy <quentin@quentinroy.fr>
2017-08-18 13:05:36 +0800

    feat: portal

--- packages/reacolo/src/index.js
+++ packages/reacolo/src/index.js
@@ -2,4 +2,5 @@ export { default as ContextSwitch } from './ContextSwitch.jsx';
 export { default as Context } from './Context.jsx';
 export { default as contextPropType } from './context-prop-type.js';
 export { default as connect } from './connect.jsx';
+export { default as portal } from './portal.js';
 export { default as bindContext } from './bind-context.js';
---
+++ packages/reacolo/src/portal.js
@@ -0,0 +1,40 @@
+/* eslint-disable react/no-multi-comp */
+import portalOrigin from './portal-origin.jsx';
+import portalOut from './portal-out.jsx';
+
+const getDisplayName = Component => Component.displayName || Component.name;
+
+// Wrap a component and returns to new component.
+// Origin is the wrapped component provided with the callbacks described by
+// callbackNames.
+// Out is an empty component (does not render), but when one of the callbacks
+// of Origin is called, that's the corresponding callback property of Out that
+// is called.
+const portal = (
+  WrappedComponent,
+  callbackNames,
+  portalId = getDisplayName(WrappedComponent)
+) => {
+  if (!portalId) {
+    throw new Error(
+      'Cannot infer an id for portal: wrapped component has no displayName and portalId is not provided'
+    );
+  }
+
+  // Map each callback name with an event name.
+  const callbackEvents = callbackNames.reduce(
+    (result, callbackName) =>
+      Object.assign(result, {
+        [callbackName]: `_portal(${portalId}):${callbackName}`
+      }),
+    {}
+  );
+
+  // Create the origin and the exit of the portal.
+  return {
+    Origin: portalOrigin(WrappedComponent, portalId, callbackEvents),
+    Out: portalOut(WrappedComponent, portalId, callbackEvents)
+  };
+};
+
+export default portal;
