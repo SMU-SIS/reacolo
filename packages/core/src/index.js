6807ace28606e799a5d7a59fcd3871d2d0cf0091
Quentin Roy <quentin@quentinroy.fr>
2017-12-19 19:19:09 +0100

    fix(core): fix import of nonexistent  `bindContext` in index

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,5 +1,5 @@
 export { default as ContextSwitch } from './components/ContextSwitch.jsx';
-export { default as Context, bindContext } from './components/Context.jsx';
+export { default as Context } from './components/Context.jsx';
 export { modelPropType } from './utils.js';
 export { default as connect } from './connect/connect.jsx';
 export { default as portal } from './portal.js';
