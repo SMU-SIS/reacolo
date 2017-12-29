6025c68c631841dad2a37bbba711bd83d1c61576
Quentin Roy <quentin@quentinroy.fr>
2017-12-29 17:23:58 +0100

    feat(core): export <Provider>

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,5 +1,6 @@
 export { default as Switch } from './components/Switch.jsx';
 export { default as Context } from './components/Context.jsx';
+export { default as Provider } from './components/Provider.js';
 export { modelPropType } from './utils.js';
 export { default as connect } from './connect/connect.jsx';
 export { default as portal } from './portal.js';
