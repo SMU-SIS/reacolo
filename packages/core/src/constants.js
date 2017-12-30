45fed787223a74c080e87fd9c2eb249093f5cf64
Quentin Roy <quentin@quentinroy.fr>
2017-12-30 14:44:31 +0100

    feat: switch the wildcase symbol to '*'
    BREAKING CHANGE: the wild case symbol has changed

--- packages/core/src/constants.js
+++ packages/core/src/constants.js
@@ -2,7 +2,7 @@
  * Symbol used for wild case in the matcher syntax.
  * @private
  */
-export const WILD_CASE = '.';
+export const WILD_CASE = '*';
 
 /**
  * Context key of the model provided by the provider.
