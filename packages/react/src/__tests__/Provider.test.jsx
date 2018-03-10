b98e0c630af81a01870a68c7e1ed386809582777
Quentin Roy <quentin@quentinroy.fr>
2018-03-10 17:44:22 +0100

    test(react): fix not working <Provider> exception test

--- packages/react/src/__tests__/Provider.test.jsx
+++ packages/react/src/__tests__/Provider.test.jsx
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
-import TestUtils from 'react-dom/test-utils';
 import propTypes from 'prop-types';
+import Enzyme, { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import Provider from '../Provider.jsx';
 import { MODEL_CONTEXT_KEY } from '../constants';
 
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
 describe('Provider', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error');
+  });
+
   // Mock the console.
   const mockConsole = () => {
-    global.console = {
-      warn: jest.fn(),
-      log: jest.fn(),
-      error: jest.fn(),
-    };
+    global.console.error.mockImplementation(() => {});
   };
+
   // Make sure the console is restored after each tests.
-  const globalConsole = global.console;
   afterEach(() => {
-    global.console = globalConsole;
+    global.console.error.mockRestore();
   });
 
   // Child cannot be a stateless function or findRenderedComponentWithType
@@ -24,54 +28,53 @@ describe('Provider', () => {
   // eslint-disable-next-line react/prefer-stateless-function
   class Child extends Component {
     render() {
-      return <div />;
+      return (
+        <div>
+          reacolo model name: {this.context[MODEL_CONTEXT_KEY].mockName}
+        </div>
+      );
     }
   }
   Child.contextTypes = {
     [MODEL_CONTEXT_KEY]: propTypes.object.isRequired,
   };
 
-  const createModel = props =>
-    Object.assign(
-      {
-        addListener: jest.fn(),
-        removeListener: jest.fn(),
-        getContext: jest.fn(),
-        getStore: jest.fn(),
-      },
-      props,
-    );
+  const createModel = props => ({
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    getContext: jest.fn(),
+    getStore: jest.fn(),
+    mockName: 'mock-model',
+    ...props,
+  });
 
   it("should add the store to the child's context", () => {
     const model = createModel();
-    const tree = TestUtils.renderIntoDocument(
+    const tree = mount(
       <Provider model={model}>
         <Child />
       </Provider>,
     );
-    const child = TestUtils.findRenderedComponentWithType(tree, Child);
-    expect(child.context[MODEL_CONTEXT_KEY]).toBe(model);
+    expect(toJSON(tree)).toMatchSnapshot();
   });
 
-  it('should throw once when receiving a new store in props', () => {
+  it('should throw if the store is changed', () => {
     // Avoid React's error message being thrown on jest's console.
     mockConsole();
 
-    const model1 = createModel();
-    const model2 = createModel();
+    const model1 = createModel({ mockName: 'mock-model-1' });
+    const model2 = createModel({ mockName: 'mock-model-2' });
 
-    const tree = TestUtils.renderIntoDocument(
+    const tree = mount(
       <Provider model={model1}>
         <Child />
       </Provider>,
     );
 
-    const child = TestUtils.findRenderedComponentWithType(tree, Child);
-
     expect(() => {
       tree.setProps({ model: model2 });
     }).toThrow();
 
-    expect(child.context[MODEL_CONTEXT_KEY]).toBe(model1);
+    expect(toJSON(tree)).toMatchSnapshot();
   });
 });
